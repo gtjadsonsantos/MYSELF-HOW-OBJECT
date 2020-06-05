6d6a728851d670b6ec1aa0cfecde9b0d1b691342
jadson179 <jadson44.santos@gmail.com>
2020-06-04 21:33:07 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+"use strict";
+/*
+ Author: Jadson Santos
+ License: MIT
+*/
+exports.__esModule = true;
+var Pearson = /** @class */ (function () {
+    function Pearson(name, age, work, email, phone, location, platforms, formations, technologies, certificates, projects, organizations) {
+        this.name = name || "Jadson dos Santos Silva";
+        this.age = age || new Date().getFullYear() - 1997;
+        this.work = work || "JavaScript Developer";
+        this.email = email || "jadosn44.santos@gmail.com";
+        this.phone = phone || "(48) 99932-8092";
+        this.location = location || { street: "Almirante Carlos da Silveira Carneiro", number: 394, zipcode: 88025350 };
+        this.platforms = platforms || [{ name: "Github", url: "http://github.com/jadson179" }, { name: "Linkedin", url: "https://gist.github.com/jadson179/www.linkedin.com/in/jadson-santos-a9aa9b145" }, { name: "porfolio", url: "http://jadsonsantos.com" }];
+        this.formations = formations || [{ name: "Analise e Desenvolvimento de Sistemas ", status: "Cursando" }, { name: "Curso Superior de Tecnologia em Gestão da Tecnologia da Informação", status: "Concluído" }, { name: "Ensino Médio", status: "Concluído" }];
+        this.technologies = technologies || ["JavaScript", "Mysql", "React", "ReactNative", "Expo", "Postgress", "Knex", "Java", "Nmap", "Docker", "Docker-Compose", "Bash", "Linux", "MacOS", "Windows", "Git", "Github", "Bitbucket", "SSH", "FTP", "VNC", "HTML 5", "CSS3", "Nodejs", "REST API", "Express", "SQL", "Markdown", "Material Design", "Font Awesome", "Sequelize ORM", "UML", "Zabbix", "Network", "Grafana", "Python"];
+        this.certificates = certificates || ["Semana OmniStack 11", "Analista de Gerenciamento de Projetos", "Deteccção avançada de problemas com zabbix", "Redes Wireless - Começando", "Redes de Computadores- Começando", "CERTIFICAÇÃO ITIL FOUNDATION 1: INTRODUÇÃO AO GERENCIAMENTO DE TI", "MYSQL I: INICIANDO SUAS CONSULTAS", "MYSQL II: CONSULTAS PODEROSAS", "JAVA I: PRIMEIROS PASSOS", "JAVA II: ORIENTAÇÃO A OBJETOS", "LÓGICA DE PROGRAMAÇÃO I: OS PRIMEIROS PROGRAMAS COM JAVASCRIPT HTML", "LÓGICA DE PROGRAMAÇÃO II: PRATIQUE COM DESENHOS, ANIMAÇÕES E UM JOGO", "Analista de Infraestrutura de TI"];
+        this.projects = projects || [{ name: "Gestão de Salas Comerciais", url: "https://github.com/jadson179/PAINEL" }, { name: "Portfólio", url: "https://github.com/jadson179/portoflio" }];
+        this.organizations = organizations || ["UNISEC LTDA", "Beco-Castelo LTDA", "UNIQUE"];
+    }
+    Pearson.prototype.setName = function (name) {
+        this.name = name;
+    };
+    Pearson.prototype.setAge = function (age) {
+        this.age = age;
+    };
+    Pearson.prototype.setWork = function (work) {
+        this.work = work;
+    };
+    Pearson.prototype.setPhone = function (phone) {
+        this.phone = phone;
+    };
+    Pearson.prototype.setEmail = function (email) {
+        this.email = email;
+    };
+    Pearson.prototype.setLocation = function (location) {
+        this.location = location;
+    };
+    Pearson.prototype.setPlatforms = function (platforms) {
+        this.platforms = platforms;
+    };
+    Pearson.prototype.setFormations = function (formations) {
+        this.formations = formations;
+    };
+    Pearson.prototype.setTechnologies = function (technologies) {
+        this.technologies = technologies;
+    };
+    Pearson.prototype.setCertificates = function (certificates) {
+        this.certificates = certificates;
+    };
+    Pearson.prototype.setProjects = function (projects) {
+        this.projects = projects;
+    };
+    Pearson.prototype.setOrganizations = function (organizations) {
+        this.organizations = organizations;
+    };
+    Pearson.prototype.getnName = function () {
+        return this.name;
+    };
+    Pearson.prototype.getAge = function () {
+        return this.age;
+    };
+    Pearson.prototype.getWork = function () {
+        return this.work;
+    };
+    Pearson.prototype.getPhone = function () {
+        return this.phone;
+    };
+    Pearson.prototype.getEmail = function () {
+        return this.email;
+    };
+    Pearson.prototype.getLocation = function () {
+        return this.location;
+    };
+    Pearson.prototype.getPlatforms = function () {
+        return this.platforms;
+    };
+    Pearson.prototype.getFormations = function () {
+        return this.formations;
+    };
+    Pearson.prototype.getTechnologies = function () {
+        return this.technologies;
+    };
+    Pearson.prototype.getCertificates = function () {
+        return this.certificates;
+    };
+    Pearson.prototype.getProjects = function () {
+        return this.projects;
+    };
+    Pearson.prototype.getOrganizations = function () {
+        return this.organizations;
+    };
+    return Pearson;
+}());
+console.log(new Pearson());
+exports["default"] = Pearson;
---
+++ index.ts
@@ -0,0 +1,136 @@
+/*
+ Author: Jadson Santos
+ License: MIT
+*/
+
+interface IProject {
+    name: string
+    url: string
+}
+interface IFormation {
+    name: string
+    status: string
+}
+interface IPlatform {
+    name: string
+    url: string
+}
+interface ILocation {
+    street: string
+    number: number
+    zipcode: number
+}
+
+class Pearson {
+    private name: string 
+    private age: number
+    private work: string 
+    private email: string  
+    private phone: string
+    private location: ILocation
+
+    private platforms: IPlatform[]
+
+    private formations: IFormation[]
+
+    private technologies: string[]
+
+    private certificates: string[]
+
+    private projects: IProject[]
+
+    private organizations: string[] 
+
+
+    constructor(name?: string, age?: number, work?: string, email?: string, phone?: string, location?: ILocation, platforms?: IPlatform[], formations?: IFormation[], technologies?: string[], certificates?: string[], projects?: IProject[], organizations?: string[]) {
+        this.name = name || "Jadson dos Santos Silva"
+        this.age = age || new Date().getFullYear() - 1997
+        this.work = work || "JavaScript Developer"
+        this.email = email || "jadosn44.santos@gmail.com"
+        this.phone = phone || "(48) 99932-8092"
+        this.location = location || { street: "Almirante Carlos da Silveira Carneiro", number: 394, zipcode: 88025350 }
+        this.platforms = platforms || [{ name: "Github", url: "http://github.com/jadson179" }, { name: "Linkedin", url: "https://gist.github.com/jadson179/www.linkedin.com/in/jadson-santos-a9aa9b145" }, { name: "porfolio", url: "http://jadsonsantos.com" }]
+        this.formations = formations || [{ name: "Analise e Desenvolvimento de Sistemas ", status: "Cursando" }, { name: "Curso Superior de Tecnologia em Gestão da Tecnologia da Informação", status: "Concluído" }, { name: "Ensino Médio", status: "Concluído" }]
+        this.technologies = technologies || ["JavaScript", "Mysql", "React", "ReactNative", "Expo", "Postgress", "Knex", "Java", "Nmap", "Docker", "Docker-Compose", "Bash", "Linux", "MacOS", "Windows", "Git", "Github", "Bitbucket", "SSH", "FTP", "VNC", "HTML 5", "CSS3", "Nodejs", "REST API", "Express", "SQL", "Markdown", "Material Design", "Font Awesome", "Sequelize ORM", "UML", "Zabbix", "Network", "Grafana", "Python"]
+        this.certificates = certificates ||  ["Semana OmniStack 11", "Analista de Gerenciamento de Projetos", "Deteccção avançada de problemas com zabbix", "Redes Wireless - Começando", "Redes de Computadores- Começando", "CERTIFICAÇÃO ITIL FOUNDATION 1: INTRODUÇÃO AO GERENCIAMENTO DE TI", "MYSQL I: INICIANDO SUAS CONSULTAS", "MYSQL II: CONSULTAS PODEROSAS", "JAVA I: PRIMEIROS PASSOS", "JAVA II: ORIENTAÇÃO A OBJETOS", "LÓGICA DE PROGRAMAÇÃO I: OS PRIMEIROS PROGRAMAS COM JAVASCRIPT HTML", "LÓGICA DE PROGRAMAÇÃO II: PRATIQUE COM DESENHOS, ANIMAÇÕES E UM JOGO", "Analista de Infraestrutura de TI"]
+        this.projects = projects || [{ name: "Gestão de Salas Comerciais", url: "https://github.com/jadson179/PAINEL" }, { name: "Portfólio", url: "https://github.com/jadson179/portoflio" }]
+        this.organizations = organizations || ["UNISEC LTDA", "Beco-Castelo LTDA", "UNIQUE"]
+    }
+
+    public setName(name: string): void {
+        this.name = name;
+    }
+    public setAge(age: number): void {
+        this.age = age;
+    }
+    public setWork(work: string): void {
+        this.work = work;
+    }
+    public setPhone(phone: string): void {
+        this.phone = phone;
+    }
+    public setEmail(email: string): void {
+        this.email = email;
+    }
+    public setLocation(location: ILocation): void {
+        this.location = location;
+    }
+    public setPlatforms(platforms: IPlatform[]): void {
+        this.platforms = platforms;
+    }
+    public setFormations(formations: IFormation[]): void {
+        this.formations = formations;
+    }
+    public setTechnologies(technologies: string[]): void {
+        this.technologies = technologies;
+    }
+    public setCertificates(certificates: string[]): void {
+        this.certificates = certificates;
+    }
+    public setProjects(projects: IProject[]): void {
+        this.projects = projects;
+    }
+    public setOrganizations(organizations: string[]): void {
+        this.organizations = organizations;
+    }
+    public getnName(): string {
+        return this.name;
+    }
+    public getAge(): number {
+        return this.age;
+    }
+    public getWork(): string {
+        return this.work;
+    }
+    public getPhone(): string {
+        return this.phone
+    }
+    public getEmail(): string {
+        return this.email;
+    }
+    public getLocation(): ILocation {
+        return this.location;
+    }
+    public getPlatforms(): IPlatform[] {
+        return this.platforms;
+    }
+    public getFormations(): IFormation[] {
+        return this.formations;
+    }
+    public getTechnologies(): string[] {
+        return this.technologies
+    }
+    public getCertificates(): string[] {
+        return this.certificates
+    }
+    public getProjects(): IProject[] {
+        return this.projects
+    }
+    public getOrganizations(): string[] {
+        return this.organizations
+    }
+}
+
+console.log(new Pearson())
+
+export default Pearson;
